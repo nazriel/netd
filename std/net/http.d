105f16456957561a8bf776fdc52fa8c218083201
Robik <szadows@gmail.com>
2011-11-14 18:11:18 +0100

    Work on HTTP

--- std/net/http.d
+++ std/net/http.d
@@ -3,11 +3,15 @@ module net.http;
 import std.socket, std.socketstream;
 import std.string, std.conv;
 
+// debug
+import std.stdio;
+
 enum HttpMethod : string
 {
     Get = "GET",
     Post = "POST"
 }
+
 
 alias HttpClient Http;
 class HttpClient
@@ -23,7 +27,9 @@ class HttpClient
         string _domain;
         string _url;
         
-        uint _port = 80;
+        ushort _httpVersion;
+        
+        ushort _port = 80;
     }
     
     this(string url, HttpMethod method = HttpMethod.Get)
@@ -42,7 +48,7 @@ class HttpClient
         // Remove http:// and https:// 
         offset = indexOf(url, "://");
         if(offset != -1)
-            url = url[offset .. $];
+            url = url[offset + 3 .. $];
         
         // Remove Anchor href
         offset = indexOf(url, "#");
@@ -66,8 +72,73 @@ class HttpClient
         offset = indexOf(_domain, ":");
         if(offset != -1)
         {
-            _port = to!uint(_domain[offset .. $]);
+            _port = to!ushort(_domain[offset .. $]);
             _domain = _domain[0 .. offset];
         }
+    }
+    
+    void open()
+    {
+        _sock = new TcpSocket(new InternetAddress(_domain, _port));
+        _ss = new SocketStream(_sock);
+    }
+    
+    // Returns raw headers for now
+    /*HttpResponse*/ string[] response()
+    {
+        _ss.writeString( to!string(_method) ~ " " ~
+                        _url ~ ["HTTP/1.0", "HTTP/1.1"][_httpVersion] ~
+                       "Host: " ~ _domain ~ "\r\n"
+                       "\r\n");
+        
+        return getHeaders();
+    }
+    
+    protected string[] getHeaders()
+    {
+        char[] line;
+        string[] headers;
+        for(;;)
+        {
+            line = _ss.readLine();
+            
+            if(line.length == 0)
+                break;
+            
+            headers ~= line.idup;   
+        }
+        
+        return headers;
+    }
+    
+    HttpMethod method() const
+    {
+        return _method;
+    }
+    
+    void method(HttpMethod method)
+    {
+        _method = method;
+    }
+    
+    ushort httpVersion() const
+    {
+        return _httpVersion;
+    }
+    
+    void httpVersion(ushort ver)
+    {
+        _httpVersion = ver ? 1 : 0;
+    }
+}
+
+version(Main)
+{
+    void main()
+    {
+        auto http = new Http("http://google.com/");
+        http.open;
+        foreach(header; http.response)
+            writeln(header);
     }
 }
